6aca87b1aba50b61a8262014694024012645c122
Oh <oss0202@naver.com>
2022-01-16 15:31:54 +0900

    Spring Boot 생성

---
+++ src/main/java/com/kakao/login/vue_springboot_login/VueSpringBootLoginApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.login.vue_springboot_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringBootLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueSpringBootLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakao/login/vue_springboot_login/VueSpringBootLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.login.vue_springboot_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringBootLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
